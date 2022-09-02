95adb100a3b8dbfb129b054b8d491e2de528efff
Tyler Neal <will1neal27@gmail.com>
2022-09-01 22:58:18 -0400

    made a delete route for apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,21 +4,34 @@ const {uuid} = require('uuidv4')
 
 router.get('/notes', (req,res) => {
   let notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'))
+
   res.json(notes)
 })
 
 router.post('/notes', (req,res) => {
   let notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'))
   let newNote = req.body
+
   newNote.id = uuid()
   let updateNotes = [...notes, newNote]
+
   fs.writeFileSync('./db/db.json', JSON.stringify(updateNotes))
+  
   res.json(updateNotes)
 })
+
+
  router.delete('/notes/:id', (req,res) => {
-  let notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'))
-  let 
- })
+  let notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+
+  const delId = req.params.id;
+  const index = notes.findIndex((note) => delId === note.id);
+
+  notes.splice(index, 1);
+  fs.writeFileSync('./db/db.json', JSON.stringify(notes));
+
+  res.json(notes)
+})
 
 
 
